5faef795ae97f727b100d3635c9c9ccb31192449
javeriakhan <jiyajo60@gmail.com>
2017-11-13 11:25:41 -0800

    multiplication assignment is done

--- Multiplication/index.js
+++ Multiplication/index.js
@@ -4,11 +4,10 @@ var bodyParser = require('body-parser');
 var app = express();
  Authentication= require('./models/authentication.js');
  HotelRegister= require('./models/hotelRegistration.js');
-//cors XML k error ko resolve krne k liye lgya h and main purpose of cors to connect frontend to backend
+ Multiplication= require('./models/multiplication.js');
 var cors = require('cors')
 cors({ credentials: true, origin: true })
 app.use(cors())
-// Parsers for POST data
 app.use(bodyParser.json(), function (err, req, res, next) {
     if (err) {
         return res.status(500).json({ error: err });
@@ -16,22 +15,16 @@ app.use(bodyParser.json(), function (err, req, res, next) {
     next();
 });
 app.use(bodyParser.urlencoded({ extended: false }));
-// connect database to mongoose
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://jiyakhan:jumbo1234@ds259325.mlab.com:59325/javeriamuneeba', {
     useMongoClient: true
-    /* other options */
 });
 app.post('/api/multiplication', function (request, response) {
     response.header('Access-Control-Allow-Origin', "*");
     num1= request.body.num1
     num2= request.body.num2
-    console.log("Hello Sir Zohaib your answer is ", num1*num2)
-    // return response.send(num1*num2);
-    // return response.send("Hello sir Zohaib your answer is ", num1*num2);
+    console.log("The multiplied answer is ", num1*num2)
 })
-
-//register school post request to mlab from server
 app.post('/api/hotelRegistration', function (request, response) {
     response.header('Access-Control-Allow-Origin', "*");
     var data = {
@@ -63,7 +56,6 @@ app.post('/api/hotelRegistration', function (request, response) {
         }
     })
 })
-//Login post request to mlab from server
 app.post('/api/login', function (request, response) {
     Authentication.findOne({ username: request.body.username}, function (err, username) {
         if (err) {
@@ -89,7 +81,6 @@ app.post('/api/login', function (request, response) {
             })
     });
 });
-//create get request to mlab from server
 app.get('/getData', function (req, res) {
     res.header('Access-Control-Allow-Origin', "*");
     HotelRegister.find(function (err, data) {
@@ -102,22 +93,17 @@ app.get('/getData', function (req, res) {
         }
     });
 })
-// When successfully connected
 mongoose.connection.on('connected to mongodb', function () {
     console.log('Mongoose default connection open to ');
 });
 
-// If the connection throws an error
 mongoose.connection.on('error', function (err) {
-    console.log('Mongoose default connection error: ' + err);
+    console.log('DB connection error: ' + err);
 });
-
-// When the connection is disconnected
 mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
+    console.log('DB connection disconnected');
 });
 var port = process.env.PORT || 8000;
 app.listen(port, function () {
-    console.log("Server run on port " + port)
+    console.log("Server is running on port " + port)
 });
-//dsffdfdfdf
